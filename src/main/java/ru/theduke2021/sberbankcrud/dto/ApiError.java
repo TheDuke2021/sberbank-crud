6741e682158c74e5de71dda17515e6cef121f3f7
TheDuke2021 <rbk_74@mail.ru>
2024-02-19 23:33:27 +0300

    add javadoc comments to ApiError

--- src/main/java/ru/theduke2021/sberbankcrud/dto/ApiError.java
+++ src/main/java/ru/theduke2021/sberbankcrud/dto/ApiError.java
@@ -11,10 +11,21 @@ public class ApiError {
     private final LocalDateTime timestamp;
     private final String message;
 
+    /**
+     * Creates an API error, obtaining error message from the given {@code exception} via {@link Exception#getMessage()}.
+     * @param status HTTP status code
+     * @param exception Exception whose message acquired by {@link Exception#getMessage()} will
+     *                 be used as this {@code ApiError} {@link #message}
+     */
     public ApiError(int status, Exception exception) {
         this(status, exception.getMessage());
     }
 
+    /**
+     * Creates an API error with the given HTTP status code and error {@code message}.
+     * @param status HTTP status code
+     * @param message Message to be included in the {@code ApiError}
+     */
     public ApiError(int status, String message) {
         this.status = status;
         this.timestamp = LocalDateTime.now();
