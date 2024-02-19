a3ee827dfa5aa7f1751b9702d7a85bbdb1939edd
TheDuke2021 <rbk_74@mail.ru>
2024-02-19 17:54:03 +0300

    Ready. Set. Go!

---
+++ src/main/java/ru/theduke2021/sberbankcrud/SberbankCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.theduke2021.sberbankcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberbankCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberbankCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/theduke2021/sberbankcrud/SberbankCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theduke2021.sberbankcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberbankCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
